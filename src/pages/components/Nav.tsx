fe445bcd2357d14bf98815e17e1497476a625029
Ha Anna <its.haanna@gmail.com>
2023-05-15 18:04:23 +0900

    Upload: SVG component, logo import into Nav

---
+++ src/pages/components/Nav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SVGComponent from './SVGComponent'
+
+export default function Nav() {
+  return (
+    <div>
+      <SVGComponent url={'/images/artemis_logo.svg'} alt={""} width={206} height={41} />
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Nav from './components/Nav'
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className={`mb-3 text-2xl font-semibold`}>
-        Artemis{' '}
-        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          &#128657;
-        </span>
-      </h1>
+    <main>
+      <Nav />
     </main>
   )
 }
