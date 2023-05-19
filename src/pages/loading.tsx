9936bfb26aed63941b75245d38234a162eb3e1f6
horus <h.horace0921@gmail.com>
2023-05-19 13:01:44 +1000

    feat: add catsprint animation

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import Layout from './components/Layout'
+import Image from 'next/image'
 import MoonLoader from 'react-spinners/MoonLoader'
 
 export default function Loading() {
   return (
     <Layout>
       <div className='flex items-center justify-center max-h-[100%] min-h-[85vh]'>
-        <MoonLoader
+        {/* <MoonLoader
           color={'#8BC34A'}
           size={150}
           aria-label='Loading Spinner'
           data-testid='loader'
-        />
+        /> */}
+        <div className='absolute h-[62.5px] left-[45%] top-[40%] inset-0 overflow-hidden'>
+          <Image
+            className='absolute w-auto h-auto animate-catSprintAnim'
+            priority={true}
+            width={100}
+            height={100}
+            alt='cat_sprint'
+            src='/images/cat_sprint.png'
+          />
+        </div>
+        <h1>Loading...</h1>
       </div>
     </Layout>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,60 @@ module.exports = {
         slideRightAndFade: {
           from: { opacity: 0, transform: 'translateX(-2px)' },
           to: { opacity: 1, transform: 'translateX(0)' }
+        },
+        catSprint: {
+          '7.6923076923%': {
+            top: '0px',
+            left: '0px'
+          },
+          '15.3846153846%': {
+            top: '-62.5px',
+            left: '0px'
+          },
+          '23.0769230769%': {
+            top: '-125px',
+            left: '0px'
+          },
+          '30.7692307692%': {
+            top: '-187.5px',
+            left: '0px'
+          },
+          '38.4615384615%': {
+            top: '-250px',
+            left: '0px'
+          },
+          '46.1538461538%': {
+            top: '-312.5px',
+            left: '0px'
+          },
+          '53.8461538462%': {
+            top: '-375px',
+            left: '0px'
+          },
+          '61.5384615385%': {
+            top: '-437.5px',
+            left: '0px'
+          },
+          '69.2307692308%': {
+            top: '-500px',
+            left: '0px'
+          },
+          '76.9230769231%': {
+            top: '-562.5px',
+            left: '0px'
+          },
+          '84.6153846154%': {
+            top: '-625px',
+            left: '0px'
+          },
+          '92.3076923077%': {
+            top: '-687.5px',
+            left: '0px'
+          },
+          '100%': {
+            top: '-750px',
+            left: '0px'
+          }
         }
       },
       animation: {
@@ -62,7 +116,8 @@ module.exports = {
           'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
         slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
         slideRightAndFade:
-          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)'
+          'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        catSprintAnim: 'catSprint 0.3939393939s infinite step-end'
       }
     }
   },
