6c1c20d3ba0a017c321051dceda81469c3650b9b
Ha Anna <its.haanna@gmail.com>
2023-05-28 18:44:36 +0900

    Refactor: formatted and refactored CSS
    - contrast issues (accessibility)
    - responsiveness for big screens

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,66 +1,82 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from 'react'
+import Layout from '../components/Layout'
 import Link from 'next/link'
 
-
 export default function SignUp() {
-    return (
-        <Layout>
-            <div className='h-scren'>
+  return (
+    <Layout>
+      <div className='min-h-[80vh] p-7 max-w-[600px] flex mx-auto'>
+        <div className='grid place-items-center mt-10 md:mt-20 mb-[150px]'>
+          <div className='grid grid-cols-12 mx-5'>
+            <h2 className='text-5xl lg:text-6xl mb-3 col-span-12 font-bold text-center'>
+              Sign In
+            </h2>
+            <h3 className='text-lg mb-5 col-span-12 text-artemis-gray'>
+              Welcome back! Join our pawsitive movement and rescue stray
+              animals!
+            </h3>
 
-                <div className="grid place-items-center mt-10 md:mt-20 lg:mt-[90px] mb-[150px]">
-                    <div className="grid grid-cols-12 mx-5 lg:w-4/12">
-                        <h2 className='text-5xl lg:text-6xl mb-3 col-span-12 text-left font-bold'>Sign In</h2>
-                        <h3 className="text-lg mb-5 col-span-12 text-gray-500">
-                        Welcome back! Join our pawsitive movement and rescue stray animals!
-                        </h3>
+            <div className='col-span-12'>
+              <label className='block overflow-hidden rounded-lg border-2 border-artemis-black px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
+                <span className='text-sm font-bold text-artemis-black'>
+                  Username
+                </span>
 
-                        <div className="col-span-12 mt-6 bg-white rounded-md">
-                            <label
-                                className="block overflow-hidden rounded-md border border-gray-500 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                            >
-                                <span className="text-sm font-bold text-gray-400">Username</span>
-
-                                <input
-                                    type="text"
-                                    id="username"
-                                    placeholder="meowuser"
-                                    className="mt-2 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-md"
-                                />
-                            </label>
-                        </div>
-
-                        <div className="col-span-12 mt-6 bg-white rounded-md">
-                            <label
-                                className="block overflow-hidden rounded-md border border-gray-500 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                            >
-                                <span className="text-sm font-bold text-gray-400">Password</span>
-
-                                <input
-                                    type="password"
-                                    id="pwduser"
-                                    placeholder=""
-                                    className="mt-2 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-md"
-                                />
-                            </label>
-                        </div>
-                        <div className="col-span-5 mt-2">
-                            <input type="checkbox" name="chkRemember" /> &nbsp;
-                            <label htmlFor="chkRemember" className="text-gray-500">Remember Me</label>
-                        </div>
-
-                        <div className="col-start-7 col-span-6 text-right mt-2 text-[#00BFFF]">
-                            Forgot Password
-                        </div>
-
-                        <button className="rounded col-span-12 my-5 text-lg bg-[#00BFFF] py-2 text-white">Login</button>
-
-                        <h3 className="text-sm px-5 mt-10 col-span-12 text-center text-gray-500">
-                         Don&apos;t have an account? <Link className="font-bold text-[#00BFFF]" href="/sign-up">Sign Up</Link>
-                        </h3>
-                    </div>
-                </div>
+                <input
+                  type='text'
+                  id='username'
+                  placeholder='meowuser'
+                  className='mt-2 w-full leading-10 border-none p-0 rounded bg-artemis-white focus:border-transparent focus:outline-none focus:ring-0'
+                />
+              </label>
             </div>
-        </Layout>
-    )
+
+            <div className='col-span-12 mt-6 bg-white rounded-md'>
+              <label className='block overflow-hidden rounded-lg border-2 border-artemis-black px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'>
+                <span className='text-sm font-bold text-artemis-black'>
+                  Password
+                </span>
+
+                <input
+                  type='password'
+                  id='pwduser'
+                  placeholder=''
+                  className='mt-2 w-full leading-10 border-none p-0 rounded bg-artemis-white focus:border-transparent focus:outline-none focus:ring-0'
+                />
+              </label>
+            </div>
+            <div className='col-span-5 mt-3'>
+              <label
+                htmlFor='chkRemember'
+                className='text-artemis-black cursor-pointer'>
+                <input
+                  type='checkbox'
+                  name='chkRemember'
+                  id="chkRemember"
+                />{' '}
+                &nbsp; Remember Me
+              </label>
+            </div>
+
+            <div className='col-start-7 col-span-6 text-right mt-3 text-artemis-blue'>
+              <Link href=''>Forgot password?</Link>
+            </div>
+
+            <button className='font-bold rounded col-span-12 my-3 mt-10 text-lg bg-artemis-blue py-4 px-5 text-white'>
+              Login
+            </button>
+
+            <p className='text-sm px-5 mt-10 col-span-12 text-center text-artemis-black'>
+              Don&apos;t have an account?{' '}
+              <Link
+                className='font-bold text-artemis-blue'
+                href='/sign-up'>
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
