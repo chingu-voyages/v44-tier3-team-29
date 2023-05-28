9d28d667457897d6a6106e755c345a713cbb0c1c
leixdd <lecias.ariel@gmail.com>
2023-05-28 14:42:10 +0800

    fix color schemes

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -5,9 +5,9 @@ import Layout from "../components/Layout";
 export default function SignUp() {
     return (
         <Layout>
-            <div className='h-screen bg-hero-gradient bg-no-repeat bg-[center_top_-50px] bg-[length:1000px] md:bg-[center_top_-50px] lg:bg-[length:1200px] lg:bg-[center_top] mb-[50px]'>
+            <div className='h-scren'>
 
-                <div className="grid place-items-center my-[150px]">
+                <div className="grid place-items-center mt-10 md:mt-20 lg:mt-[90px] mb-[150px]">
                     <div className="grid grid-cols-12 mx-5 lg:w-4/12">
                         <h2 className='text-5xl lg:text-6xl mb-3 col-span-12 text-left font-bold'>Sign In</h2>
                         <h3 className="text-lg mb-5 col-span-12 text-gray-500">
@@ -48,14 +48,14 @@ export default function SignUp() {
                             <label htmlFor="chkRemember" className="text-gray-500">Remember Me</label>
                         </div>
 
-                        <div className="col-start-7 col-span-6 text-right mt-2 text-green-800">
+                        <div className="col-start-7 col-span-6 text-right mt-2 text-[#00BFFF]">
                             Forgot Password
                         </div>
 
-                        <button className="rounded col-span-12 my-5 text-lg bg-[#8BC34A] py-2 text-white">Login</button>
+                        <button className="rounded col-span-12 my-5 text-lg bg-[#00BFFF] py-2 text-white">Login</button>
 
                         <h3 className="text-sm px-5 mt-10 col-span-12 text-center text-gray-500">
-                         Doesn't have an account? <a className="font-bold text-green-500" href="/sign-up">Sign Up.</a>
+                         Doesn't have an account? <a className="font-bold text-[#00BFFF]" href="/sign-up">Sign Up.</a>
                         </h3>
                     </div>
                 </div>
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -5,9 +5,9 @@ import Layout from "../components/Layout";
 export default function SignUp() {
     return (
         <Layout>
-            <div className='h-scren bg-hero-gradient bg-no-repeat bg-[center_top_-50px] bg-[length:1000px] md:bg-[center_top_-50px] lg:bg-[length:1200px] lg:bg-[center_top] mb-[50px]'>
+            <div className='h-scren'>
 
-                <div className="grid place-items-center my-[150px]">
+                <div className="grid place-items-center mt-10 md:mt-20 mb-[150px]">
                     <div className="grid grid-cols-12 mx-5 lg:w-8/12 xl:w-5/12">
                         <h2 className='text-5xl lg:text-6xl mb-3 col-span-12 text-left font-bold'>Sign Up</h2>
                         <h3 className="text-lg mb-5 col-span-12 text-gray-500">
@@ -74,13 +74,13 @@ export default function SignUp() {
                             </label>
                         </div>
 
-                        <h3 className="text-sm px-5 mt-5 col-span-12 sm:col-span-7  text-gray-500">
-                        By clicking the "Register" button below, you acknowledge that you have read, understood, and agree to be bound by the following <a className="font-bold text-green-500" href="#">Terms and Conditions</a>
+                        <h3 className="text-sm mt-5 px-5 lg:px-10  col-span-12 text-gray-500 text-center">
+                        By clicking the "Register" button below, you acknowledge that you have read, understood, and agree to be bound by the following <a className="font-bold text-[#00BFFF]" href="#">Terms and Conditions</a>
                         </h3>
-                        <button className="rounded col-span-12 sm:col-span-5 my-5 text-lg bg-[#8BC34A] py-2 text-white">Sign Up</button>
+                        <button className="rounded col-span-12 my-5 text-lg bg-[#00BFFF] py-2 text-white">Sign Up</button>
 
                         <h3 className="text-sm px-5 mt-10 col-span-12 text-center text-gray-500">
-                         Do you have an account? <a className="font-bold text-green-500" href="/sign-in">Login here.</a>
+                         Do you have an account? <a className="font-bold text-[#00BFFF]" href="/sign-in">Login here.</a>
                         </h3>
                     </div>
                 </div>
