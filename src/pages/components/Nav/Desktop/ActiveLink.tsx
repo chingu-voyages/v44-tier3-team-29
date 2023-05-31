525c406f5c36ae647b6a02e17742fe5d599ee2c2
Ha Anna <its.haanna@gmail.com>
2023-05-31 13:26:53 +0900

    Fix: naming change

--- src/pages/components/Nav/Desktop/ActiveLink.tsx
+++ src/pages/components/Nav/Desktop/ActiveLink.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function ActiveLink({ href, content }: { href: string, content: string }) {
+export default function ActiveLink({ url, content }: { url: string, content: string }) {
   const router = useRouter()
-  const style = router.pathname === href ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
+  const style = router.asPath === url ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
 
   return (
     <Link
-      href={href}
+      href={url}
       className={style}>
       {content}
     </Link>
