a50ca4d5a0089c7a84582eca70b9a86c858d11de
Ha Anna <its.haanna@gmail.com>
2023-05-31 13:22:15 +0900

    Test: testing changes

--- src/pages/components/Nav/Desktop/ActiveLink.tsx
+++ src/pages/components/Nav/Desktop/ActiveLink.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function ActiveLink({ href, content }: { href: string, content: string }) {
-  const { asPath } = useRouter();
-  const style = asPath === href ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
+  const router = useRouter()
+  const style = router.pathname === href ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
 
   return (
     <Link
