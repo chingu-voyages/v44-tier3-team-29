aa64a4b5769f0a6fff27818be93a79b48a40ee3d
Ha Anna <its.haanna@gmail.com>
2023-05-31 13:16:33 +0900

    Fix: destructuring asPath to try to get rid of a build error

--- src/pages/components/Nav/Desktop/ActiveLink.tsx
+++ src/pages/components/Nav/Desktop/ActiveLink.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function ActiveLink({ href, content }: { href: string, content: string }) {
-  const router = useRouter();
-  const style = router.asPath === href ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
+  const { asPath } = useRouter();
+  const style = asPath === href ? 'mx-3 [text-shadow:_0_0_10px_#8BC34A]' : 'mx-3 hover:[text-shadow:_0_0_10px_#8BC34A] ease-in-out duration-300'
 
   return (
     <Link
