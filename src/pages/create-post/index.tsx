ecc61f47213c6a3246b7bfe55e609b944f3d47f9
Ha Anna <its.haanna@gmail.com>
2023-05-31 18:16:47 +0900

    Upload: form v 1.0

--- src/pages/create-post/index.tsx
+++ src/pages/create-post/index.tsx
@@ -4,42 +4,44 @@ import Layout from '../components/Layout'
 export default function CreatePost() {
   return (
     <Layout>
-      <div className='mb-[150px] relative z-[1] p-5 mt-10 flex flex-col items-center'>
+      <div className='mb-[150px] p-5 mt-10 flex flex-col mx-auto items-center max-w-[1000px] w-full'>
         <h2 className='text-5xl lg:text-6xl mb-8 mt-16 text-center'>
           Create Post
         </h2>
-        <form>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input type="text" id="title" name="title" required />
+        <form className="flex flex-wrap gap-10 w-full justify-between">
+          <div className="flex flex-col basis-5/12 justify-between">
+            <div className="flex flex-col">
+              <label htmlFor="title" className="font-bold text-lg">Title</label>
+              <input className="border-2 border-artemis-black rounded p-2 mt-2 mb-4" type="text" id="title" name="title" required />
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="location" className="font-bold text-lg">Location</label>
+              <input className="border-2 border-artemis-black rounded p-2 mt-2 mb-4" type="text" id="location" name="location" required />
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="short-description" className="font-bold text-lg">Short Description</label>
+              <input className="border-2 border-artemis-black rounded p-2 mt-2 mb-4" type="text" id="short-description" name="short-description" maxLength={200} required />
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="tags" className="font-bold text-lg">Tags</label>
+              <input className="border-2 border-artemis-black rounded p-2 mt-2 mb-4" type="text" id="tags" name="tags" />
+            </div>
+
+            <div className="flex flex-col">
+              <label htmlFor="file-upload" className="font-bold text-lg">Image</label>
+              <input type="file" id="file-upload" name="file-upload" accept="image/*" className="file:border-2 file:border-artemis-black file:rounded file:bg-artemis-white file:color-artemis-black file:p-2 file:cursor-pointer file:mr-5 file:hover:bg-artemis-yellow file:ease-in-out file:duration-300 mt-2 mb-4" />
+            </div>
           </div>
 
-          <div>
-            <label htmlFor="location">Location:</label>
-            <input type="text" id="location" name="location" required />
+          <div className="flex flex-col basis-6/12">
+            <label htmlFor="long-description" className="font-bold text-lg">Long Description</label>
+            <textarea className="border-2 border-artemis-black rounded p-2 mt-2 mb-4 h-full"  id="long-description" name="long-description" required></textarea>
           </div>
 
-          <div>
-            <label htmlFor="short-description">Short Description:</label>
-            <input type="text" id="short-description" name="short-description" maxLength="200" required />
-          </div>
-
-          <div>
-            <label htmlFor="tags">Tags:</label>
-            <input type="text" id="tags" name="tags" />
-          </div>
-
-          <div>
-            <label htmlFor="file-upload">File Upload:</label>
-            <input type="file" id="file-upload" name="file-upload" accept="image/*" />
-          </div>
-
-          <div>
-            <label htmlFor="long-description">Long Description:</label>
-            <textarea id="long-description" name="long-description" required></textarea>
-          </div>
-
-          <button type="submit">Submit</button>
+          <button className="ml-auto flex-none border-artemis-blue border-2 bg-artemis-blue py-2 px-4 text-artemis-white rounded hover:drop-shadow-lg ease-in-out duration-300 mr-2" type="submit">Create Post</button>
         </form>
       </div>
     </Layout>
