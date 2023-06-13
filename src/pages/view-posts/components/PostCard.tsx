a5a0a0c46932e13c3f2942e98788dbdac924cc9b
Ha Anna <its.haanna@gmail.com>
2023-06-13 23:26:25 +0900

    Upload: PostCard

--- src/pages/view-posts/components/PostCard.tsx
+++ src/pages/view-posts/components/PostCard.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import { TPostData } from '../../../lib/types/PostDataTypes'
 import Image from "next/image"
+import Link from "next/link"
+
+const generateTagBorderColors = (array: string[]) => {
+  const colors = ["border-artemis-green", "border-artemis-yellow", "border-artemis-blue"];
+  const borderColors = [];
+  for (let i = 0; i < array.length; i++) {
+    borderColors.push(colors[i % colors.length]);
+  }
+  return borderColors;
+};
 
 export default function PostCard({ data }: {data: TPostData}) {
-const { title, location, shDesc, tags, image, lgDesc } = data
+  const { title, location, shDesc, tags, image } = data;
+  const formattedTags = tags.split(",").map(tag => tag.trim().toLowerCase());
+
+  const tagsHtml = formattedTags.map((tag, i) => {
+          const borderColors = generateTagBorderColors(formattedTags);
+          const style = "mr-2 border-2 py-0.5 px-2 px-2 rounded" + " " + borderColors[i];
+          return (<li key={tag} className={style}><Link href={tag}>#{tag}</Link></li>)
+  })
 
   return (
     <div className="border-2 border-artemis-black rounded-md p-5 ">
-      <div className="border-2 border-artemis-black rounded-md">
-        <Image src={image} alt="" className="object-cover" width={300} height={200} />
+      <div className="border border-artemis-black rounded">
+        <Image src={image} alt="" className="object-cover " width={300} height={200} />
       </div>
       <h3 className="text-3xl mt-2">{title}</h3>
-      <span className="color-artemis-gray">{location}</span>
-      <p>{shDesc}</p>
-      
+      <span className="color-artemis-gray opacity-50">{location}</span>
+      <p className="py-3 text-lg">{shDesc}</p>
+      <ul className="flex">{tagsHtml}</ul>
     </div>
   )
 }
--- src/pages/view-posts/index.tsx
+++ src/pages/view-posts/index.tsx
@@ -3,17 +3,56 @@ import Layout from '../components/Layout'
 import PostCard from './components/PostCard'
 import { TPostData } from '../../lib/types/PostDataTypes'
 
+// TODO: check if img is necessary when creating a post
+const addPlaceholders = (data: TPostData[]) => {
+  const placeholders = [
+    '/images/placeholder-green.jpg',
+    '/images/placeholder-yellow.jpg',
+    '/images/placeholder-blue.jpg'
+  ]
+  let counter = 0
+  for (let i = 0; i < data.length; i++) {
+    if (counter < placeholders.length) {
+      if (!data[i].image) {
+        data[i].image = placeholders[counter]
+        counter++
+      }
+    } else {
+      counter = 0
+    }
+  }
+  return data
+}
+
 export default function ViewPosts() {
   const [postsData, setPostsData] = React.useState<TPostData[]>([
-  {
-   title: "Cat",
-   location: "Seoul",
-   shDesc: "Cute and small",
-   tags: "Cat, small, hungry",
-   image: "/images/placeholder-green.jpg",
-   lgDesc: "Cute small cat, goes meow meow"
-  }
+    {
+      title: 'Cat',
+      location: 'Seoul',
+      shDesc: 'Cute and small',
+      tags: 'Cat, small, hungry',
+      image: '',
+      lgDesc: 'Cute small cat, goes meow meow'
+    },
+    {
+      title: 'Cat',
+      location: 'Seoul',
+      shDesc: 'Cute and small',
+      tags: 'Cat, small, hungry',
+      image: '',
+      lgDesc: 'Cute small cat, goes meow meow'
+    },
+    {
+      title: 'Cat',
+      location: 'Seoul',
+      shDesc: 'Cute and small',
+      tags: 'Cat, small, hungry',
+      image: '',
+      lgDesc: 'Cute small cat, goes meow meow'
+    }
   ])
+
+  addPlaceholders(postsData)
 
   React.useEffect(() => {
     // fetch posts
@@ -23,7 +62,12 @@ export default function ViewPosts() {
     <Layout>
       <div className='mb-[150px] p-5 mt-10 flex flex-col items-center'>
         <h2 className='text-5xl lg:text-6xl mb-8 mt-16 text-center'>Posts</h2>
-        {postsData.map((data, i) => <PostCard data={data} key={i}/>)}
+        {postsData.map((data, i) => (
+          <PostCard
+            data={data}
+            key={i}
+          />
+        ))}
       </div>
     </Layout>
   )
